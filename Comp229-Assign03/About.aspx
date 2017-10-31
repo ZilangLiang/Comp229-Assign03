3f613564f3a408af40bfcb70133cadffd320e989
b1598 <b1598@DESKTOP-T77LB1C>
2017-10-31 14:45:48 -0400

    Cannot launch this page.

--- Comp229-Assign03/About.aspx
+++ Comp229-Assign03/About.aspx
@@ -4,12 +4,27 @@
     <div class="row">
         <div class="col-md-6">
             <h2>Your information</h2>
-        </div>
-        <div class="col-md-6">
-            <h2>Your courses' details</h2>
-            <table runat="server" style="width:auto;">
-                <tr></tr>
+            <table border="1">
+                <tr>
+                    <td>Course ID</td>
+                    <td>
+                        <asp:Label ID="lblcourseid" runat="server"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                <td>Student ID</td>
+                <td>
+                    <asp:Label ID="lblstudentid" runat="server"></asp:Label>
+                </td>
+                </tr>
+                <tr>
+                    <td>Grade</td>
+                    <td>
+                        <asp:Label ID="lblgrade" runat="server"></asp:Label>
+                    </td>
+                </tr>
             </table>
+
         </div>
 
 
--- Comp229-Assign03/About.aspx.cs
+++ Comp229-Assign03/About.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Comp229_Assign03
 {
@@ -11,7 +14,34 @@ namespace Comp229_Assign03
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                string productID = Request.QueryString["StudentID"];
 
+                if(productID == null)
+                {
+                    Response.Redirect("Default.aspx");
+                }
+
+                string cs = ConfigurationManager.ConnectionStrings["Comp229Assign03ConnectionString5"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand("Enrollments", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlParameter parameter = new SqlParameter("StudentID", productID);
+                    cmd.Parameters.Add(parameter);
+                    con.Open();
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        while (rdr.Read())
+                        {
+                            lblstudentid.Text = rdr["StudentID"].ToString();
+                            lblgrade.Text = rdr["Grade"].ToString();
+                            lblcourseid.Text = rdr["CourseID"].ToString();
+                        }
+                    }
+                }
+            }
         }
     }
 }
